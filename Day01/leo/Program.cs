54cc594f078820597f940d1a25cfc37e6d5630ef
le.labat <le.labat@criteo.com>
2022-12-01 17:12:37 +0100

    Leo - Day01

---
+++ Day01/leo/Program.cs
@@ -0,0 +1,28 @@
+﻿var inputFileContent = File.ReadAllLines("input.txt");
+
+var capacity = 3;
+
+var priorityQueue = new PriorityQueue<int, int>(capacity);
+var sum = 0;
+
+foreach (var inputRow in inputFileContent)
+{
+    if (inputRow == string.Empty)
+    {
+        if (priorityQueue.Count == capacity)
+        {
+            priorityQueue.EnqueueDequeue(sum, sum);
+        }
+        else
+        {
+            priorityQueue.Enqueue(sum, sum);
+        }
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(inputRow);
+    }
+}
+
+Console.WriteLine(priorityQueue.UnorderedItems.Select(x => x.Element).Sum());
