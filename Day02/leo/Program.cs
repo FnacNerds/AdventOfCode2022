6c67285d47fdaae53c0571cecdf0f05ca3eb4fe5
le.labat <le.labat@criteo.com>
2022-12-02 11:38:38 +0100

    leo day 2

---
+++ Day02/leo/Program.cs
@@ -0,0 +1,49 @@
+﻿var inputFileContent = File.ReadAllLines("input.txt");
+
+var letterToScore = new Dictionary<string, int>()
+{
+    { "A", 1 },
+    { "B", 2 },
+    { "C", 3 },
+};
+
+var letterToWinCondition = new Dictionary<string, string>()
+{
+    {"A", "B"},
+    {"B", "C"},
+    {"C", "A"},
+};
+
+var letterToLooseCondition = letterToWinCondition.ToDictionary(x => x.Value, x => x.Key);
+
+var score = 0;
+
+foreach (var inputRow in inputFileContent)
+{
+    if (inputRow == string.Empty)
+    {
+        continue;
+    }
+
+    var splitted = inputRow.Split(" ");
+
+    var whatToDo = splitted[1];
+    var opponent = splitted[0];
+
+    switch (whatToDo)
+    {
+        case "X":
+            score += letterToScore[letterToLooseCondition[opponent]];
+            break;
+        case "Y":
+            score += 3;
+            score += letterToScore[opponent];
+            break;
+        case "Z":
+            score += 6;
+            score += letterToScore[letterToWinCondition[opponent]];
+            break;
+    }
+}
+
+Console.WriteLine(score);
